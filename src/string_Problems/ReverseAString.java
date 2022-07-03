bdf4f3c9dde337d7e90c28e58b77b11299c51755
Pradeep Kumar <pradeepkumar1988@gmail.com>
2022-07-03 13:15:39 +0530

    Reverse A String program added

---
+++ src/string_Problems/ReverseAString.java
@@ -0,0 +1,25 @@
+package string_Problems;
+
+public class ReverseAString {
+
+	public static void main(String[] args) {
+		
+		String str = "My name is Pradeep";
+		
+		System.out.println("Reverse of string "+str+" is: "+ReverseString(str));
+		
+		String str2 = "Hello123Hello";
+		System.out.println("Reverse of string "+str2+" is: "+ReverseString(str2));
+
+	}
+	
+	public static String ReverseString(String str) {
+		
+		String revStr="";
+		for(int i=str.length()-1; i>=0;i--) {
+			revStr = revStr+str.charAt(i);
+		}
+		return revStr;
+	}
+
+}
